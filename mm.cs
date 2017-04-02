c42958d9d08bea8c1b2dadc8c8fd7e40f82b098c
Grant Eaton <grantman100@gmail.com>
2017-04-02 18:04:26 -0400

    read in file and create file based on students

--- mm.cs
+++ mm.cs
@@ -1,24 +1,48 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-using System.IO;}
+using System.IO;
 
 public class Hello
 {
-   public static void Main()
-      {
+   public static void Main(string[] args)
+   {
 
-		StreamReader input = new StreamReader("items.csv");
-		StreamWriter output = new StreamWriter("sorted.txt");
+		StreamReader input = new StreamReader(args[0]);
+		List<string []> students = new List<string []> ();
+
 		while (!input.EndOfStream) {
-			string line = input.ReadLine();
-			string[] toks = line.Split(',');
-			foreach (int value in values) {
-				output.Write(value + " ");
-}
-	output.WriteLine();
-			input.Close();
-			output.Close();
+			string student = input.ReadLine();
+			string[] fields = student.Split('\t');
+
+			if(File.Exists(args[1])){
+			    File.Delete(args[1]);
+			}
+
+			File.Copy(args[1],fields[1]);
+			string studentFile = File.ReadAllText(args[1]); 
+
+			foreach (String field in fields) {
+				studentFile.Replace("<<NAME>>", fields[0]);
+				studentFile.Replace("<<COURSE>>", fields[2]);
+				studentFile.Replace("<<ID>>", fields[1]);
+				studentFile.Replace("<<TOTAL>>", fields[12]);
+				studentFile.Replace("<<SUBTOTAL>>", fields[11]);
+				studentFile.Replace("<<LATEDEDUCTIONS>>", fields[6]);
+				studentFile.Replace("<<DUE>>", fields[0]);
+				studentFile.Replace("<<SUBMITTED>>", fields[4]);
+				studentFile.Replace("<<MINUTESLATE>>", fields[5]);
+				studentFile.Replace("<<P1>>", fields[7]);
+				studentFile.Replace("<<P1COMMENTS>>", fields[8]);
+				studentFile.Replace("<<P2>>", fields[9]);
+				studentFile.Replace("<<P2COMMENTS>>", fields[10]);
+
+			}
+		
+		Console.WriteLine(studentFile);
+		File.WriteAllText(args[1], studentFile);
 		}
+	input.Close();
+		
+	}	
 }
-	
