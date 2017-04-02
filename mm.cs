7be872c6f132febb1ee3f69a26e6c5948cf8d481
Grant Eaton <grantman100@gmail.com>
2017-04-02 19:22:54 -0400

    finished c# practice

--- mm.cs
+++ mm.cs
@@ -3,44 +3,42 @@ using System.Collections.Generic;
 using System.Text;
 using System.IO;
 
-public class Hello
+public class hmwk04
 {
    public static void Main(string[] args)
    {
 
 		StreamReader input = new StreamReader(args[0]);
-		List<string []> students = new List<string []> ();
-
+		input.ReadLine();
+		
 		while (!input.EndOfStream) {
 			string student = input.ReadLine();
 			string[] fields = student.Split('\t');
 
-			if(File.Exists(args[1])){
-			    File.Delete(args[1]);
+			if(File.Exists(fields[1])){
+			    File.Delete(fields[1]);
 			}
 
 			File.Copy(args[1],fields[1]);
-			string studentFile = File.ReadAllText(args[1]); 
-
-			foreach (String field in fields) {
-				studentFile.Replace("<<NAME>>", fields[0]);
-				studentFile.Replace("<<COURSE>>", fields[2]);
-				studentFile.Replace("<<ID>>", fields[1]);
-				studentFile.Replace("<<TOTAL>>", fields[12]);
-				studentFile.Replace("<<SUBTOTAL>>", fields[11]);
-				studentFile.Replace("<<LATEDEDUCTIONS>>", fields[6]);
-				studentFile.Replace("<<DUE>>", fields[0]);
-				studentFile.Replace("<<SUBMITTED>>", fields[4]);
-				studentFile.Replace("<<MINUTESLATE>>", fields[5]);
-				studentFile.Replace("<<P1>>", fields[7]);
-				studentFile.Replace("<<P1COMMENTS>>", fields[8]);
-				studentFile.Replace("<<P2>>", fields[9]);
-				studentFile.Replace("<<P2COMMENTS>>", fields[10]);
-
-			}
+			string studentFileStr = File.ReadAllText(args[1]); 
+			StringBuilder studentFileBldr = new StringBuilder(studentFileStr);
+			
+			studentFileBldr.Replace("<<NAME>>", fields[0]);
+			studentFileBldr.Replace("<<COURSE>>", fields[2]);
+			studentFileBldr.Replace("<<ID>>", fields[1]);
+			studentFileBldr.Replace("<<TOTAL>>", fields[12]);
+			studentFileBldr.Replace("<<SUBTOTAL>>", fields[11]);
+			studentFileBldr.Replace("<<LATEDEDUCTION>>", fields[6]);
+			studentFileBldr.Replace("<<DUE>>", fields[3]);
+			studentFileBldr.Replace("<<SUBMITTED>>", fields[4]);
+			studentFileBldr.Replace("<<MINUTESLATE>>", fields[5]);
+			studentFileBldr.Replace("<<P1>>", fields[7]);
+			studentFileBldr.Replace("<<P1COMMENTS>>", fields[8]);
+			studentFileBldr.Replace("<<P2>>", fields[9]);
+			studentFileBldr.Replace("<<P2COMMENTS>>", fields[10]);
 		
-		Console.WriteLine(studentFile);
-		File.WriteAllText(args[1], studentFile);
+			//Console.WriteLine(studentFile);
+			File.WriteAllText(fields[1], studentFileBldr.ToString());
 		}
 	input.Close();
 		
