84ebc1f04c802a790a91c06f0ae617bd14967062
Grant Eaton <grantman100@gmail.com>
2017-04-02 19:43:55 -0400

    added comments to CS file

--- mm.cs
+++ mm.cs
@@ -8,17 +8,23 @@ public class hmwk04
    public static void Main(string[] args)
    {
 
+	   // skip first line of file because its just headers
 		StreamReader input = new StreamReader(args[0]);
 		input.ReadLine();
 		
+		//go through each line
 		while (!input.EndOfStream) {
+			// split each line by tabs
 			string student = input.ReadLine();
 			string[] fields = student.Split('\t');
 
+			//delete file if exists; mainly used for testing
 			if(File.Exists(fields[1])){
 			    File.Delete(fields[1]);
 			}
 
+			// copy template file, then make SB of template file to replace 
+			// keywords in file with
 			File.Copy(args[1],fields[1]);
 			string studentFileStr = File.ReadAllText(args[1]); 
 			StringBuilder studentFileBldr = new StringBuilder(studentFileStr);
@@ -37,7 +43,7 @@ public class hmwk04
 			studentFileBldr.Replace("<<P2>>", fields[9]);
 			studentFileBldr.Replace("<<P2COMMENTS>>", fields[10]);
 		
-			//Console.WriteLine(studentFile);
+			//dump SB
 			File.WriteAllText(fields[1], studentFileBldr.ToString());
 		}
 	input.Close();
